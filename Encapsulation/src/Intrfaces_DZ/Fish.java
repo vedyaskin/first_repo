1370da0b3f33201774190a3fc2400d055a069018
vedyaskin <k.vedyaskin@gmail.com>
2024-06-25 21:57:31 +0300

    DZ

--- Encapsulation/src/Intrfaces_DZ/Fish.java
+++ Encapsulation/src/Intrfaces_DZ/Fish.java
@@ -2,13 +2,13 @@ package Intrfaces_DZ;
 
 public class Fish implements Swimmable, Flyable{
     String species;
-    boolean can_swim, // указывает умеет ли рыба плавать или нет
+    boolean canSwim, // указывает умеет ли рыба плавать или нет
                         // (есть одна рыба, котрая не умеет плавать, она ходит по дну)
             flying; // указывает летающая рыба или нет
 
-    public Fish(String species, boolean can_swim, boolean flying) {
+    public Fish(String species, boolean canSwim, boolean flying) {
         this.species = species;
-        this.can_swim = can_swim;
+        this.canSwim = canSwim;
         this.flying = flying;
     }
     @Override
@@ -18,7 +18,7 @@ public class Fish implements Swimmable, Flyable{
     }
     @Override
     public void swim() {
-        if ( can_swim ) System.out.println( "Умеет плавать." );
+        if ( canSwim ) System.out.println( "Умеет плавать." );
         else System.out.println( "Не умеет плавать.");
     }
 }
