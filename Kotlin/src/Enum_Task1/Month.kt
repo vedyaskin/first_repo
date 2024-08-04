75e301ece355123b5ded04b6955acf2e2bdae48c
vedyaskin <k.vedyaskin@gmail.com>
2024-08-04 17:50:24 +0300

    DZ

--- Kotlin/src/Enum_Task1/Month.kt
+++ Kotlin/src/Enum_Task1/Month.kt
@@ -1,19 +1,4 @@
 package Enum_Task1
 
 data class Month(var month: String, var season: Season) {
-
-    val january = "январь"
-    val february = "февраль"
-    val march = "март"
-    val april = "апрель"
-    val may = "май"
-    val june = "июнь"
-    val july = "июль"
-    val august = "август"
-    val september = "сентябрь"
-    val october = "октябрь"
-    val november = "ноябрь"
-    val december = "декабрь"
-
-
 }
